cabb9e43f70f2d1c3bf76b54b0ac5b828bd441fd
xznilesh <xznilesh@gmail.com>
2026-09-03 04:14:14 +0530

    Add shared XZ Recruiter brand component

---
+++ app/components/Brand.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Brand({ href = '/', compact = false }) {
+  const image = <Image
+    className="brand-logo"
+    src="/xzrecruiter-logo.svg"
+    alt="XZ Recruiter"
+    width={compact ? 170 : 196}
+    height={compact ? 55 : 63}
+    priority
+  />;
+
+  return href ? <Link className="brand" href={href} aria-label="XZ Recruiter home">{image}</Link> : <div className="brand" aria-label="XZ Recruiter">{image}</div>;
+}
