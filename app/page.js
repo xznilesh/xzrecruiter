2ffaaf008838d55999be3568d67b0bca502f1c31
xznilesh <xznilesh@gmail.com>
2026-09-03 00:43:01 +0530

    Size XZ Recruiter wordmark consistently

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-const Brand = () => <div className="brand"><img className="brand-logo" src="/xzrecruiter-logo.png" alt="XZ Recruiter" /></div>;
+const Brand = () => <div className="brand"><img className="brand-logo" src="/xzrecruiter-logo.png" alt="XZ Recruiter" width="205" height="30" /></div>;
 
 export default function Home(){
   return <main>
